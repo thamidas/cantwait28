ef8c9a8d496f631ba39772a9434090300a8114d8
Przybylski Kamil <przybylskikamil663@gmail.com>
2022-01-23 14:51:05 +0100

    add save

---
+++ lib/models/item_model.dart
@@ -0,0 +1,10 @@
+class ItemModel {
+  ItemModel({
+    required this.title,
+    required this.imageURL,
+    required this.releaseDate,
+  });
+  final String title;
+  final String imageURL;
+  final DateTime releaseDate;
+}
